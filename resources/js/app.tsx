007aee72e823a39f3fbcae678c9c11fb49edbb58
trickstercode-dev <trickstercode.dev@gmail.com>
2025-11-27 10:56:35 +0700

    Initial Commit

---
+++ resources/js/app.tsx
@@ -0,0 +1,45 @@
+import '@primer/react-brand/fonts/fonts.css';
+import '@primer/react-brand/lib/css/main.css';
+
+import { ThemeProvider } from '@primer/react-brand';
+
+import '../css/app.css';
+
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { initializeTheme } from './hooks/use-appearance';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => (title ? `${title} - ${appName}` : appName),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./pages/${name}.tsx`,
+            import.meta.glob('./pages/**/*.tsx'),
+        ),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+
+        root.render(
+            <StrictMode>
+                <ThemeProvider
+                    colorMode={'dark'}
+                    style={{
+                        backgroundColor: 'var(--brand-color-canvas-default)',
+                    }}
+                >
+                    <App {...props} />
+                </ThemeProvider>
+            </StrictMode>,
+        );
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
+
+// This will set light / dark mode on load...
+initializeTheme();
